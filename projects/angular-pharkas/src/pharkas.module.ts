2d8fb21731e5d5aff16327a1176a1cea78b2f83f
Max Battcher <me@worldmaker.net>
2022-02-14 23:43:09 -0500

    Add an NgModule to help appease Angular's stupid module loader

---
+++ projects/angular-pharkas/src/pharkas.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core'
+import { PharkasComponent } from 'angular-pharkas'
+
+@NgModule({
+  declarations: [PharkasComponent],
+  exports: [PharkasComponent],
+})
+export class PharkasModule {}
